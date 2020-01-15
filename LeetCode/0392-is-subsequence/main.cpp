a39636099f38add6559941e05aad296fe92589a1
JHyunB <whdgus489@gmail.com>
2020-01-15 14:22:45 +0900

    Add leetcode problem

---
+++ LeetCode/0392-is-subsequence/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    class Solution {
+    public:
+        bool isSubsequence(string s, string t) {
+            int count = 0;
+            if (s.length() == 0)
+                return true;
+            for (int i = 0, j = 0; i < s.length(), j < t.length();)
+            {
+                if (s[i] != t[j])
+                {
+                    j++;
+                }
+                else
+                {
+                    i++;
+                    j++;
+                    count++;
+                    if (count == s.length())
+                        return true;
+                }
+            }
+            return false;
+        }
+    };
+}
